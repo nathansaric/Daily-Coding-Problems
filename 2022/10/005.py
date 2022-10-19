b5fb9839c2dbc15ef750e9917487d105fb3efdd9
Nathan Saric <70985863+nathansaric@users.noreply.github.com>
2022-10-19 19:02:03 -0400

    Update 005.py

--- 2022/10/005.py
+++ 2022/10/005.py
@@ -1,4 +1,4 @@
-# DAILY CODING PROBLEM FOR 10/19/2022
+# DAILY CODING PROBLEM FOR 10/19/2022 
 
 # cons(a, b) constructs a pair, and car(pair) and cdr(pair) 
 # returns the first and last element of that pair. 
